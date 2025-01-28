0053c8ed91561bbcf25ec203312311a3237651b5
Alexandre Uzan <alexandreuzan9@gmail.com>
2025-01-28 13:11:19 +0100

    ADD morse Flag 5 is validate

--- socket_client.py
+++ socket_client.py
@@ -93,6 +93,16 @@ def solve_math_expression(statement):
         return None
 
 
+MORSE_DICT = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+    '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+    '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+    '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+    '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+    '--..': 'Z', '-----': '0', '.----': '1', '..---': '2',
+    '...--': '3', '....-': '4', '.....': '5', '-....': '6',
+    '--...': '7', '---..': '8', '----.': '9'
+}
 
 def flag1(conn):
     """
@@ -183,6 +193,41 @@ def flag4(conn, statement):
         print(f"Erreur inattendue dans flag4 : {e}")
         return None
 
+def flag5(conn, statement):
+    """
+    Décoder un message en morse donné en hexadécimal et répondre en majuscules.
+    """
+    try:
+        #Extraire le message hexadécimal
+        hex_message = statement.split(":")[-1].strip()
+        print(f"Message hexadécimal : {hex_message}")
+
+        #Décoder de l'hexadécimal en morse
+        morse_message = bytes.fromhex(hex_message).decode('utf-8')
+        print(f"Message morse : {morse_message}")
+
+        #Convertir le morse en texte
+        """
+        On met les mots dans une liste, 
+        puis on les sépare par 2 espaces pour les mots et 1 espace pour les lettres.
+        """
+        words = morse_message.split("  ")
+        decoded_message = []
+        for word in words:
+            letters = word.split(" ")
+            decoded_word = ''.join(MORSE_DICT.get(letter, '?') for letter in letters)
+            decoded_message.append(decoded_word)
+        final_message = ' '.join(decoded_message).upper()
+
+        print(f"Message décodé : {final_message}")
+
+        conn.sendall(final_message.encode())
+        print(f"Réponse envoyée : {final_message}")
+        return wait_answer(conn)
+
+    except Exception as e:
+        print(f"Erreur dans le traitement de flag5 : {e}")
+        return None
 
 
 def main():
@@ -207,11 +252,19 @@ def main():
         else:
             flag4_result = flag4(conn, flag3_result)
 
+
+        if not flag4_result:
+            print("Erreur : Impossible de résoudre Flag 4. Passage au suivant.")
+            flag5_result = None
+        else:
+            flag5_result = flag5(conn, flag4_result)
+
         print("\nRécapitulatif des FLAGS :")
         print(f"FLAG 1 : {flag1_result}")
         print(f"FLAG 2 : {flag2_result}")
         print(f"FLAG 3 : {flag3_result}")
         print(f"FLAG 4 : {flag4_result}")
+        print(f"FLAG 5 : {flag5_result}")
     finally:
         conn.close()
         print("Connexion fermée.")
